05b2c837aaee7bd9fe88e3d4d6f932eec7f7c9e7
Ruletik <iam@ruletik.org>
2025-07-29 23:07:32 +0500

    Redid animation for the letters using vector position lerp function

--- ruletik.js
+++ ruletik.js
@@ -2,21 +2,10 @@ import * as THREE from 'three'
 import gsap from 'gsap'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
-import { Pane } from 'tweakpane'
-
-
-console.log(document.fonts.status)
 
 //Mapper Function
 const mapper = gsap.utils.mapRange(0, 1000, 0, 1)
 
-//TweakPane Gui
-const pane = new Pane()
-const paneFolder = pane.addFolder({ 
-                                        title: 'Ruletik GUI' 
-                                    })
-
-//Canvas Element
 const canvas = document.querySelector('canvas.webgl')
 const clock = new THREE.Clock()
 //Sizes
@@ -24,21 +13,25 @@ const sizes = {
                     width: window.innerWidth,
                     height: window.innerHeight
                 }
-//Renderer
 const renderer = new THREE.WebGLRenderer({
                                                 canvas: canvas
                                             })
 renderer.setSize(sizes.width, sizes.height)
-//renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))//Setting pixel ratio
 renderer.setPixelRatio(2)//Setting pixel ratio
 renderer.setClearColor(0xD5CDC0) //Instead of black background
 
-//Texture Loader
 const textureLoader = new THREE.TextureLoader()
-
-//Scene
 const scene = new THREE.Scene()
 
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // FOV vertical angle, aspect ratio with/height
+camera.position.set(0,0,2)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableRotate = false
+controls.zoomSpeed = 0.75
 
 const gltfLoader = new GLTFLoader()
 
@@ -76,7 +69,7 @@ const preloaderEyes = {
     ]
 }
 
-const preloaderEyesGroup = new THREE.Group()
+
 const eyesCirclesArray = [
     new THREE.Group(),
     new THREE.Group(),
@@ -90,105 +83,132 @@ preloaderTexture.repeat.set(1 / preloaderEyes.tilesCountX, 1 / preloaderEyes.til
 preloaderTexture.offset.x = offsetX
 preloaderTexture.offset.y = offsetY
 
-//First Loop is Circle
-for (let c = 0; c < 3; c++) {
 
-        const angleDiff = 2 * Math.PI / preloaderEyes.count[c]
+
+scene.add(drawEyesCircles(preloaderEyes, preloaderTexture))
+
+
+
+
+function rotateEyes () {
+    eyesCirclesArray[0].rotation.z += 0.0015
+    eyesCirclesArray[1].rotation.z -= 0.001
+    eyesCirclesArray[2].rotation.z += 0.0005
+}
+
+
+let currentTile = 0
+let lastTile = 133
+
+function scrollEyeTexture () {
+    currentTile++
+    if (currentTile == lastTile) { currentTile = 0 }
+    let offsetX = (currentTile % preloaderEyes.tilesCountX) / preloaderEyes.tilesCountX
+    let offsetY = (preloaderEyes.tilesCountY - Math.floor(currentTile / preloaderEyes.tilesCountX) - 1) / preloaderEyes.tilesCountY
+
+    preloaderTexture.offset.x = offsetX
+    preloaderTexture.offset.y = offsetY
+}
+
+
+const frequency = 60
+let currentFrame = 0
+let secondsPassed = 0
+let siteReady = false
+
+
+//Animation Loop
+const updateFrame = () => {
+
+
+    rotateEyes() // Rotate Eye Cirecles Around
+    scrollEyeTexture() //Scroll Texture In Eyes
+
+    currentFrame++
+    //Time in Seconds
+    if (currentFrame == frequency ) {
+
+        currentFrame = 0
+        secondsPassed = Math.round(clock.getElapsedTime())
+        console.clear()
+        console.log(secondsPassed + ' Seconds')
+
+        if (secondsPassed == 1) {
+            siteReady = true
+        }
+
+
+
+    }
+
+    if (siteReady == true) {
+        camera.position.lerp({x: camera.position.x, y: camera.position.y, z: 0.5},0.075)
+    }
+
+    if (camera.position.z <= 0.51) {
+        siteReady = false
+    }
+
+
+
+
+
+
+
+    //Update
+    controls.update() // Update Controls
+    renderer.render(scene, camera) //Render Current Frame
+    window.requestAnimationFrame(updateFrame) //Request Next Frame
+
+}
+updateFrame() //Start First Frame
+
+
+
+
+function drawEyesCircles (eyes, texture) {
+
+    const preloaderEyesGroup = new THREE.Group()
+    //First Loop is Circle
+    for (let c = 0; c < 3; c++) {
+
+        const angleDiff = 2 * Math.PI / eyes.count[c]
 
 
 
         //Second loop for eyes
-        for (let i = 0; i < preloaderEyes.count[c]; i++) {
+        for (let i = 0; i < eyes.count[c]; i++) {
 
-            preloaderEyes.geometry = new THREE.PlaneGeometry(preloaderEyes.width * preloaderEyes.scale[c], preloaderEyes.height * preloaderEyes.scale[c])
+            eyes.geometry = new THREE.PlaneGeometry(eyes.width * eyes.scale[c], eyes.height * eyes.scale[c])
 
 
             let temp = new THREE.Mesh(
-                preloaderEyes.geometry,
-                new THREE.MeshBasicMaterial( { map: preloaderTexture, transparent: true })
+                eyes.geometry,
+                new THREE.MeshBasicMaterial( { map: texture, transparent: true })
             )
 
-            temp.position.x = Math.sin(angleDiff * i) * preloaderEyes.spaceBetween[c]
-            temp.position.y = Math.cos(angleDiff * i) * preloaderEyes.spaceBetween[c]
+            temp.position.x = Math.sin(angleDiff * i) * eyes.spaceBetween[c]
+            temp.position.y = Math.cos(angleDiff * i) * eyes.spaceBetween[c]
 
             temp.rotateZ(-i*angleDiff)
-            
+
 
             eyesCirclesArray[c].add(temp)
 
         }
 
         preloaderEyesGroup.add(eyesCirclesArray[c])
+    }
+
+    return preloaderEyesGroup
 }
 
-scene.add(preloaderEyesGroup)
-
-//Camera Settings
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // FOV vertical angle, aspect ratio with/height
-camera.position.set(0,0,2)
-scene.add(camera)
-
-const controls = new OrbitControls(camera, canvas)
-
-let spriteObj = { currentTile: 0 }
-let step = 1
-
-
-gsap.fromTo(spriteObj, {
-    currentTile: 0,
-}, {
-    currentTile: 133,
-    yoyo: true,
-    repeat: -1,
-    ease: "steps(133)",
-    duration: 5
-})
-
-
-document.addEventListener('DOMContentLoaded', e => {
-
-    setTimeout(() => {
-        gsap.to(camera.position, {
-            z: 0.4,
-            duration: 2,
-            ease: "power4.inOut",
-        })
-    }, 1200)
-
-})
 
 
 
-//Animation Loop Function
-const tick = () => {
-
-    const elapsedTime = clock.getElapsedTime() //Built in function in seconds since start
-
-    eyesCirclesArray[0].rotation.z += 0.0015
-    eyesCirclesArray[1].rotation.z -= 0.001
-    eyesCirclesArray[2].rotation.z += 0.0005
-    
-        
-    let offsetX = (spriteObj.currentTile % preloaderEyes.tilesCountX) / preloaderEyes.tilesCountX
-    let offsetY = (preloaderEyes.tilesCountY - Math.floor(spriteObj.currentTile / preloaderEyes.tilesCountX) - 1) / preloaderEyes.tilesCountY
-    //console.log(spriteObj.currentTile)
-
-    preloaderTexture.offset.x = offsetX
-    preloaderTexture.offset.y = offsetY
 
 
-    camera.lookAt(new THREE.Vector3()) //Empty Vector3 method resul in 0 0 0  Vector, basically center of the scene
-
-    //Render Function
-    renderer.render(scene, camera) //by default all objects will appear at center of the scene in 0 0 0 coordinates, meaning camera will be at the center too
-
-    //Request next frame
-    window.requestAnimationFrame(tick) // requestAnimationFrame purpose is to call a function on the next frame, thus we will need to call this function each frame
-    controls.update()
-}
-tick()
-
-
+//----------------- UTILITY ------------//
 
 //Check if browser tab was not active
 document.addEventListener("visibilitychange", () => {
@@ -199,9 +219,7 @@ document.addEventListener("visibilitychange", () => {
     }
 });
 
-
-
-//Resize Function
+//Resize Canvas when resizing main windows
 window.addEventListener('resize', () => {
     
     //Update Sizes
